647817d674b6bfc2b527be6fd87a365002423d37
Jakob Schmid <jakobschmid25.12@gmail.com>
2023-01-07 18:48:30 +0100

    add disgusting title text

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,13 +9,41 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Center(
           child: Column(
-        children: [
-          Text("Flappy Cavity",
-              style: TextStyle(), textAlign: TextAlign.center),
-          ElevatedButton(onPressed: () {}, child: Text("new")),
-          ElevatedButton(onPressed: () {}, child: Text("records"))
-        ],
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                ScaleAnimatedText("Flappy Cavity",
+                    textStyle: const TextStyle(
+                        fontSize: 60,
+                        fontFamily: "EastSeaDokdo",
+                        color: Colors.red),
+                    duration: const Duration(milliseconds: 500),
+                    scalingFactor: 0.8),
+                ScaleAnimatedText("Flappy Cavity",
+                    textStyle: const TextStyle(
+                        fontSize: 60,
+                        fontFamily: "EastSeaDokdo",
+                        color: Colors.red),
+                    duration: const Duration(milliseconds: 500),
+                    scalingFactor: 0.8),
+                ScaleAnimatedText("",
+                    textStyle: TextStyle(
+                        fontSize: 60,
+                        fontFamily: "EastSeaDokdo",
+                        color: Colors.black.withOpacity(0)),
+                    duration: const Duration(milliseconds: 300))
+              ],
+              isRepeatingAnimation: true,
+              repeatForever: true,
+              pause: const Duration(milliseconds: 0),
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text("new")),
+          ElevatedButton(onPressed: () {}, child: const Text("records"))
+        ],
       )),
     );
   }
