c05e9c9ed6e2f3c90d66d37db5dc70b93a9807cd
Jakob Schmid <jakobschmid25.12@gmail.com>
2023-01-12 11:24:21 +0100

    add text style to record cards

--- lib/widgets/record_list_tile.dart
+++ lib/widgets/record_list_tile.dart
@@ -9,15 +9,16 @@ class RecordListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DateTime date = _record.date.toLocal();
+    TextStyle style = const TextStyle(fontSize: 18);
     return Card(
       color: Colors.white,
       child: Row(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("time: ${_record.duration.inSeconds}s"),
-          Text("#barriers: ${_record.barriersPassed}"),
-          Text("date: ${date.year}-${date.month}-${date.day}")
+          Text("time: ${_record.duration.inSeconds}s", style: style),
+          Text("#barriers: ${_record.barriersPassed}", style: style),
+          Text("date: ${date.year}-${date.month}-${date.day}", style: style)
         ],
       ),
     );
